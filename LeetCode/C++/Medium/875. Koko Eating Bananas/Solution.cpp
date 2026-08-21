df7f362a25b96382b1d7b5ceacef2a2d39a11d25
Pradeep5Shukla <shuklapradeep29689@gmail.com>
2026-08-21 19:02:34 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/875. Koko Eating Bananas/Solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+
+    int findmax(vector<int> arr){
+        int maxval = INT_MIN;
+        for(int i =0;i<arr.size();i++){
+                maxval = max(maxval,arr[i]);
+        }
+        return maxval;
+    }
+    int hr(vector<int> arr , int h ,int n){
+        int totalhr = 0;
+        for(int i = 0;i<n ; i++){
+            totalhr+=ceil((double)arr[i]/(double)h);
+        }
+        return totalhr;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int left = 0;
+        int right = findmax(piles);
+        int ans = INT_MAX;
+        while(right>=left){
+            int mid = left + (right-left)/2;
+            int totalhr = hr(piles,mid,n);
+            if(totalhr<=h){
+                ans = mid;
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+
+    }
+};
