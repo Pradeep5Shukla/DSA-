83b866f88cf98df706fd7843f91427f20db4f9c9
Pradeep5Shukla <shuklapradeep29689@gmail.com>
2026-08-27 00:30:02 +0530

    Add Medium C++ Solution for 1408. Find the Smallest Divisor Given a Threshold from LeetCode

---
+++ LeetCode/C++/Medium/1408. Find the Smallest Divisor Given a Threshold/Solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int sumbyD(vector <int>& arr , int div){
+        int sum = 0;
+        int n = arr.size();
+        for(int i  = 0; i<n ; i++){
+            sum+=ceil((double)arr[i]/(double)div);
+        }
+        return sum;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1 ;
+        int ans = 0; 
+        int high = *max_element(nums.begin(),nums.end());
+        while(high>=low){
+            int mid = low+(high-low);
+            if(sumbyD(nums,mid) <= threshold){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
