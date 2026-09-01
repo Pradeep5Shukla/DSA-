76004c96432251c7271b62448f6876f0d1aa8004
Pradeep5Shukla <shuklapradeep29689@gmail.com>
2026-09-01 18:48:41 +0530

    Add Easy C++ Solution for 28. Find the Index of the First Occurrence in a String from LeetCode

--- LeetCode/Patterns/Two Pointers/28. Find the Index of the First Occurrence in a String/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/28. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.size()<haystack.size()) return 0;
+        if(haystack.size()<needle.size()) return -;
         for (int i = 0; i <= haystack.size() - haystack.size(); i++) {
             if (haystack.substr(i, needle.size()) == needle) {
                 return i;
