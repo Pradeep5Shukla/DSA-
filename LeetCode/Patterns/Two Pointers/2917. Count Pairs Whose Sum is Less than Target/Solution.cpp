d50faac2fab6b9fee50a88b4b20bbc4e5dd021a4
Pradeep5Shukla <shuklapradeep29689@gmail.com>
2026-09-15 01:21:07 +0530

    Add Easy C++ Solution for 2917. Count Pairs Whose Sum is Less than Target from LeetCode

--- LeetCode/Patterns/Two Pointers/2917. Count Pairs Whose Sum is Less than Target/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/2917. Count Pairs Whose Sum is Less than Target/Solution.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        // int n = nums.size();
-        // int count = 0;
-        // for(int i  = 0 ; i <n ; i++){
-        //     for(int j = i+1 ; j<n ; j++){
-        //         if(nums[i]+nums[j] < target) count++;
-        //     }
-        // }
-        // return count;
-
-        sort(nums.begin(), nums.end());
-        int left = 0, right = nums.size() - 1;
+        int n = nums.size();
         int count = 0;
-        
-        while (left < right) {
-            if (nums[left] + nums[right] < target) {
-                count+=(right-left);
-                left++;
-            } else {
-                right--;
+        for(int i  = 0 ; i <n ; i++){
+            for(int j = i+1 ; j<n ; j++){
+                if(nums[i]+nums[j] < target) count++;
             }
         }
         return count;
+
+        // sort(nums.begin(), nums.end());
+        // int left = 0, right = nums.size() - 1;
+        // int count = 0;
+        
+        // while (left < right) {
+        //     if (nums[left] + nums[right] < target) {
+        //         count+=(right-left);
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // return count;
     }
 };
